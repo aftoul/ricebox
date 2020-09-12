f57ee689a910595989b3254dfcae282940b2e2bd
AbdelfattahToulaoui <aft.power.2014@gmail.com>
2020-09-12 14:30:49 +0100

    Fixed some errors

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 
 # Install required packages
-sudo apt install compton connman-gtk xss-lock redshift notification-daemon libnotify-bin ranger ark python3 python3-pip evince okular zenity xbacklight i3lock i3blocks git flock
+sudo apt install compton connman-gtk xss-lock redshift notification-daemon libnotify-bin ranger ark python3 python3-pip evince okular zenity xbacklight i3lock i3blocks git fonts neovim feh fonts-hack fonts-font-awesome wget rxvt-unicode
 
 # Install packages to build i3-gaps from source
 sudo apt install gcc make autoconf automake pkg-config flex bison check
 
-sudo apt install libpango1.0-dev libpangocairo-1.0-0 libcairo2-dev libglib2.0-dev librsvg2-dev libstartup-notification0-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb1-dev libxcb-xkb-dev libxcb-randr0-dev libxcb-xinerama0-dev libxcb-util-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xrm-dev
+sudo apt install libpango1.0-dev libpangocairo-1.0-0 libcairo2-dev libglib2.0-dev librsvg2-dev libstartup-notification0-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb1-dev libxcb-xkb-dev libxcb-randr0-dev libxcb-xinerama0-dev libxcb-util0-dev libxcb-shape0-dev libxcb-cursor-dev libxcb-keysyms1-dev libyajl-dev libev-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xrm-dev
 
 
 # Some python deps
@@ -19,15 +19,14 @@ git clone https://github.com/Airblader/i3
 cd i3
 mkdir build && cd build
 ../configure --disable-check
-make && make install
-cd ..
+make && sudo make install
+cd ../..
 
 
 # Install the dotfiles
-cp -av home/. ~/
+cp -arv home/. ~/
 
 # Install vimplug in neovim
-
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 nvim -c ':PlugInstall
